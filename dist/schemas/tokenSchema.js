d5aa777d15f76e6a403ad94d0a1ed19d903b5825
Anandhu Anil <anandhu.anil@qburst.com>
2023-04-03 16:48:32 +0530

    Removed dist

--- dist/schemas/tokenSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const refreshTokenSchema = new mongoose_1.Schema({
-    token: { type: String, required: true },
-    validity: String
-});
-exports.default = refreshTokenSchema;
